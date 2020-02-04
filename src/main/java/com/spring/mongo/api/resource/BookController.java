d2153b6821fcc1d1c41c1d136e28c9fe72974e97
Ajay Dhami <ajay08dhami18@gmail.com>
2020-02-04 23:19:58 +0545

    Logging Functionality Added

--- src/main/java/com/spring/mongo/api/resource/BookController.java
+++ src/main/java/com/spring/mongo/api/resource/BookController.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,9 +19,12 @@ public class BookController {
 	@Autowired
 	private BookRepository bookRepository;
 
+	private static final Logger LOGGER= LoggerFactory.getLogger(BookController.class);
+
 	@PostMapping("/addBook")
 	@ApiOperation(value = "Save new book to Bookstore", notes = "Add new book to bookstore")
 	public String saveBook(@RequestBody Book book) {
+		LOGGER.info("Inside saveBook(). Book: "+book);
 		bookRepository.save(book);
 		return "Added book with id: " + book.getId();
 	}
@@ -33,12 +38,14 @@ public class BookController {
 	@GetMapping("/listBooks/{id}")
 	@ApiOperation(value = "Find particular Book by ID", notes = "Provides an id to look up specific book from bookstore")
 	public Optional<Book> getBook(@PathVariable int id) {
+		LOGGER.info("Inside getBook(). Book Particular ID to show is: "+id);
 		return bookRepository.findById(id);
 	}
 
 	@DeleteMapping("/deleteBook/{id}")
 	@ApiOperation(value = "Delete Book by providing ID", notes = "Provides an id to delete specific book from bookstore")
 	public String deleteBook(@PathVariable int id) {
+		LOGGER.info("Inside deleteBook(). Book Particular ID to delete is: "+id);
 		bookRepository.deleteById(id);
 		return "Book deleted with id : " + id;
 	}
